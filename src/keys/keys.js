73c0181c5c463a9bbaddaf81600a1a30c140e2d9
Alon Rubin <alonrubin2@gmail.com>
2021-04-05 20:46:09 +0300

    heroku upload

---
+++ src/keys/keys.js
@@ -0,0 +1,13 @@
+
+
+const keys = {
+    ID: 'AKIAIBIBL2TWFJCDIEWA',
+    secret: '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',
+    bucketName: 'top-bucket'
+}
+
+const mapKey = {
+    google: 'AIzaSyAWxsYTmh_ZaowZjqM-GWj59L4iCbMcY1A'
+}
+
+module.exports = {keys, mapKey};
